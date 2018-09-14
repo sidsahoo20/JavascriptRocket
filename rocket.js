4a6440c140964ceeea9662afa6ec271af61cc1b7
Sidhartha Sekhar Sahoo <sidsahoo20@gmail.com>
2018-09-14 21:47:52 +0530

    Launch coding done
    launch bug fix

--- rocket.js
+++ rocket.js
@@ -1,14 +1,28 @@
-var timer;
-var countDownStart = 10;
+var timer = null;
+//var countDownStart = 10;
 
-function phaseChange(){
-	document.body.className = "initialPhase2";  
-    timer = setInterval(function(){
-    	countDownStart--;
-        document.getElementById("countDown").innerHTML = countDownStart;
-        if(countDownStart < 0){
-        	clearInterval(timer);
-        	document.body.className = "initialPhase3";
-        }
-    },1000);
-  }
+var phaseChange = function(phase) {
+    document.body.className = "initialPhase"+phase;
+    clearInterval(timer);
+    var countDownStart = 10;
+    document.getElementById('countDown').innerHTML = countDownStart;
+    if (phase == 2){
+        timer = setInterval(function()  {
+            countDownStart = countDownStart-1;
+            document.getElementById('countDown').innerHTML = countDownStart;
+            if (countDownStart <= 0) {
+                phaseChange(3);
+            }
+        }, 500);
+    } else if (phase == 3) {
+        var message = setTimeout(function(){
+            var randumNum = Math.round(Math.random()*10);
+            console.log(randumNum);
+            if (randumNum > 3) {
+                phaseChange(4);
+            }else{
+                phaseChange(5);
+            }
+        }, 3000);
+    }
+}
